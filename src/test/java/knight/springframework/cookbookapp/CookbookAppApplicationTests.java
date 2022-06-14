2df516a697d8492b0d3479bc7826412c381af7e0
TheFirstHokage <kendrickutwhite@gmail.com>
2022-06-14 06:22:38 -0700

    first commit

---
+++ src/test/java/knight/springframework/cookbookapp/CookbookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package knight.springframework.cookbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
